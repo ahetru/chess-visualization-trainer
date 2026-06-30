b37ced1893d3f7006ff784ffd06e18354855926c
ahetru <axel.a.hetru@gmail.com>
2026-06-30 13:17:08 +0200

    feat: bootstrap fullstack Spring Boot + React Vite setup

---
+++ backend/src/main/java/com/ahetru/innerchess/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ahetru.innerchess.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "InnerChess backend is running";
+    }
+}
