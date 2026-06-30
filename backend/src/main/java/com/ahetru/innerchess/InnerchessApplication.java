40f7ad881e11350668e4be882594b9f142035a4b
ahetru <axel.a.hetru@gmail.com>
2026-06-30 12:36:40 +0200

    feat: add basic backend spring boot structure

---
+++ backend/src/main/java/com/ahetru/innerchess/InnerchessApplication.java
@@ -0,0 +1,13 @@
+package com.ahetru.innerchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerchessApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ahetru/innerchess/InnerchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahetru.innerchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
